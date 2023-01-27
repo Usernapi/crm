da23b4eeb9909c06bfdf029cf9a610b2322b53b6
Your Name <yourgmail address>
2023-01-26 23:49:30 -0600

    ffrr

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const cors = require("cors");
 
 // Middlewares
 const app = express();
+
+var corsOptions = {
+  origin: "*", // Reemplazar con dominio
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
